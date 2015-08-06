0d2060b05c3b8dbf2522323af4b51514ef813954
Doug Emery <doug@emeryit.com>
2015-08-05 20:09:38 -0700

    Add data_dir to config

--- bin/box_config.rb
+++ bin/box_config.rb
@@ -1,10 +1,12 @@
 require_relative 'environment'
 require 'sp-boxer'
 
+
 SPBoxer.configure do |config|
-  config.shootlist_mapping        = File.join data_dir, 'MAPPING_ShootList2ShotConfig.txt'
-  config.shot_details             = File.join data_dir, 'shot_details.yml'
-  config.metadata_template        = File.join data_dir, 'metadata_template.json'
-  config.illumination_details_dir = File.join data_dir, 'IlluminationDetails'
-  config.camera_setups_dir        = File.join data_dir, 'CameraSetups'
+  config.data_dir                 = File.expand_path '../../data', __FILE__
+  config.shootlist_mapping        = File.join config.data_dir, 'MAPPING_ShootList2ShotConfig.txt'
+  config.shot_details             = File.join config.data_dir, 'shot_details.yml'
+  config.metadata_template        = File.join config.data_dir, 'metadata_template.json'
+  config.illumination_details_dir = File.join config.data_dir, 'IlluminationDetails'
+  config.camera_setups_dir        = File.join config.data_dir, 'CameraSetups'
 end
--- lib/sp-boxer/configuration.rb
+++ lib/sp-boxer/configuration.rb
@@ -1,5 +1,6 @@
 module SPBoxer
   class Configuration
+    attr_accessor :data_dir
     attr_accessor :shot_details
     attr_accessor :shootlist_mapping
     attr_accessor :metadata_template
