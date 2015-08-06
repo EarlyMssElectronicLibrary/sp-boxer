211a56e5e60ad326c81a8133d06439d70664c36c
Doug Emery <emeryr@upenn.edu>
2015-08-05 22:47:31 -0400

    Various

--- bin/box_config.rb
+++ bin/box_config.rb
@@ -1,14 +1,10 @@
 require_relative 'environment'
 require 'sp-boxer'
 
-data_dir                 = File.expand_path '../../data', __FILE__
-
-shot_details_config      = File.join data_dir, 'shot_details.yml'
-shootlist_mapping_config = File.join data_dir, 'MAPPING_ShootList2ShotConfig.txt'
-metadata_template_json   = File.join data_dir, 'metadata_template.json'
-
 SPBoxer.configure do |config|
-  config.shootlist_mapping = shootlist_mapping_config
-  config.shot_details      = shot_details_config
-  config.metadata_template = metadata_template_json
+  config.shootlist_mapping        = File.join data_dir, 'MAPPING_ShootList2ShotConfig.txt'
+  config.shot_details             = File.join data_dir, 'shot_details.yml'
+  config.metadata_template        = File.join data_dir, 'metadata_template.json'
+  config.illumination_details_dir = File.join data_dir, 'IlluminationDetails'
+  config.camera_setups_dir        = File.join data_dir, 'CameraSetups'
 end
--- lib/sp-boxer/configuration.rb
+++ lib/sp-boxer/configuration.rb
@@ -3,5 +3,7 @@ module SPBoxer
     attr_accessor :shot_details
     attr_accessor :shootlist_mapping
     attr_accessor :metadata_template
+    attr_accessor :illumination_details_dir
+    attr_accessor :camera_setups_dir
   end
 end
